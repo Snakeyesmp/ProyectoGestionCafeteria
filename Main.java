0769e795746bffa3a76460be5c721c29abda1294
Snakeyesmp <snakeyes271101@gmail.com>
2023-02-16 10:19:42 +0100

    Primera subida de programa a GitHub
    He añadido un main con un System.out.println y he cambiado un poco lo que aparece en el README.md

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Esto es una prueba para ver si se sube el código a GitHub :D");
+    }
+}
---
+++ out/production/ProyectoGestionCafeteria/README.md
@@ -0,0 +1,4 @@
+# ProyectoGestionCafeteria
+Tercer intento de subir un proyecto de Java a Github para trabajar conjuntamente
+
+A ver si esto se sube a GitHub
